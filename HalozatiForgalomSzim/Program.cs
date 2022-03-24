e0f764713172aba37d1d62b6db64235596462839
szdanika <danikaszab@gmail.com>
2022-03-24 11:11:27 +0100

    Add project files.

---
+++ HalozatiForgalomSzim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HalozatiForgalomSzim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
