c5b51b219b23f4d99060fcd3576f43b5d3356fa6
szdanika <danikaszab@gmail.com>
2022-03-24 11:38:59 +0100

    interface

---
+++ HalozatiForgalomSzim/Interfaces/IHalozatiEszkoz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalozatiForgalomSzim.Interfaces
+{
+    internal interface IHalozatiEszkoz
+    {
+        public int networkAddres { get; set; }
+        public int bytesRecived { get; set; }
+        public int bytesSent { get; set; }
+        //KapcsolódóEszközök tulajdonságok
+        public void Send(string sender, string reciver, int bytes);
+        public void Recive(string sender, string reciver, int bytes);
+    }
+}
