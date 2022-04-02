65b09d392e6a3b1a175562727b485688d14ca3a4
szdanika <danikaszab@gmail.com>
2022-04-02 18:42:33 +0200

    Base_NetwrokTool

---
+++ HalozatiForgalomSzim/NetworkTool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalozatiForgalomSzim
+{
+    abstract internal class NetworkTool : Interfaces.IHalozatiEszkoz
+    {
+        public  int networkAddres { get; set; }
+        public  int bytesRecived { get; set; }
+        public  int bytesSent { get; set; }
+
+        public abstract void Recive(string sender, string reciver, int bytes);
+        public abstract void Send(string sender, string reciver, int bytes);
+    }
+}
---
+++ HalozatiForgalomSzim/NetworkTools/Router.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalozatiForgalomSzim.NetworkTools
+{
+    internal class Router : NetworkTool
+    {
+        public override void Recive(string sender, string reciver, int bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Send(string sender, string reciver, int bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
